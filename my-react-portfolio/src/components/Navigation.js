f211fff50863786575376cd4393a5ca910e31783
Shane <shanep42@gmail.com>
2022-12-05 13:02:08 -0600

    sets up styling so it actually applies

--- my-react-portfolio/src/components/Navigation.js
+++ my-react-portfolio/src/components/Navigation.js
@@ -1,23 +1,15 @@
-const styles = {
-    ul: {
-        display: 'flex',
-        listStyle: 'none',
-        justifyContent: 'space-between'
-    }
-}
-
 function Navigation({ currentPage, setCurrentPage }) {
 
     return (
         <nav>
-            <ul style={styles.ul}>
+            <ul>
                 <li
-                    className={currentPage == 'About Me' ? 'navItem active' : 'navItem'}
+                    className={currentPage === 'About Me' ? 'navItem active' : 'navItem'}
                     onClick={() => setCurrentPage('About Me')}>
                     About Me
                 </li>
                 <li
-                    className={currentPage == 'Portfolio' ? 'navItem active' : 'navItem'}
+                    className={currentPage === 'Portfolio' ? 'navItem active' : 'navItem'}
                     onClick={() => setCurrentPage('Portfolio')}>Portfolio</li>
                 <li
                     className={currentPage === "Contact" ? 'navItem active' : 'navItem'}
