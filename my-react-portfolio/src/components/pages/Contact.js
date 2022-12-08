727b025426dd80503c086bffd625310d9ad4f134
shanep42 <107292088+shanep42@users.noreply.github.com>
2022-12-08 15:35:04 -0600

    Changes contact page to use required form element

--- my-react-portfolio/src/components/pages/Contact.js
+++ my-react-portfolio/src/components/pages/Contact.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { validateEmail } from '../../utils/emailValidate';
 
 const Contact = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const {name, email, message} = formState;
+
+
     return (
         <main id="contact">
-            <h1 className="lg-heading">
+            {/* <h1 className="lg-heading">
                 Contact <span className="text-secondary">Me</span>
             </h1>
             <h2 className="sm-heading">
@@ -18,8 +28,36 @@ const Contact = () => {
                         <span className="text-secondary">Github:</span> shanep42
                     </div>
                 </a>
-            </div>
-
+            </div> */}
+            <form>
+                        <div>
+                            <label htmlFor='Name'>Name:</label>
+                            <br></br>
+                            <input 
+                                type='text'
+                                defaultValue={name}
+                                name='Name'
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor='email'>Email Adress:</label>
+                            <br></br>
+                            <input 
+                                type='email'
+                                defaultValue={email}
+                                name='email'
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor='message'>Message:</label>
+                            <br></br>
+                            <input 
+                                type='text'
+                                defaultValue={message}
+                            />
+                        </div>
+                        <button type='submit'>Submit</button>
+                    </form>
         </main>
     )
 }
