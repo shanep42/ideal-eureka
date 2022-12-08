d9aed1c26d9be95ed5f074c97c762fc3f5f57ea1
shanep42 <107292088+shanep42@users.noreply.github.com>
2022-12-08 17:07:32 -0600

    Messes with the file structure to try and fix constant issues

--- my-react-portfolio/src/components/pages/Resume.js
+++ my-react-portfolio/src/components/pages/Resume.js
@@ -1,12 +1,16 @@
 import React from 'react'
+// TODO: Put resume in proper place once someone can help me structure this for deployment
+// import resume from
+import {Link} from 'react-router-dom'
 
 function Resume() {
     return (
         <div>           
             <div className="boxes">
-                <a href='../../../documents/Resume'>
-                    <div> Download as PDF</div>
-                </a>
+                {/* <Link to={resume} download>
+                    Download as PDF
+                </Link> */}
+                <div>Download as PDF</div>
             </div>
         </div>
     )
