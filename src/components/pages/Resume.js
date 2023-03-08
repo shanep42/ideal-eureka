73ed849363eb387718db2df53a24cb8bc331d69b
Shane <shanep42@gmail.com>
2023-03-08 14:02:22 -0600

    fixes href typo

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,7 +6,7 @@ import React from 'react'
 function Resume() {
     return (
         <div>
-            <a target={'_blank'} href={https://docs.google.com/document/d/11jH_21n0dxRibMY_5AYTd1TmB0oPu5O1yVpU6-T1aJk/edit?usp=sharing}>
+            <a target={'_blank'} href='https://docs.google.com/document/d/11jH_21n0dxRibMY_5AYTd1TmB0oPu5O1yVpU6-T1aJk/edit?usp=sharing'>
                 <div className="boxes">
                     <div>Google Doc</div>
                 </div>
